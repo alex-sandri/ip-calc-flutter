48e7c9a81af9fd6f61d02b223348cade88e75d0a
Alex Sandri <me@alexsandri.com>
2020-08-18 14:52:10 +0200

    Add isValid method in SubnetMask class

--- lib/subnet_mask.dart
+++ lib/subnet_mask.dart
@@ -3,4 +3,13 @@ class SubnetMask
   final String subnetMask;
 
   SubnetMask(this.subnetMask);
+
+  bool isValid() {
+    const String subnetMaskRegExpPart = "(128|192|224|240|248|252|254|255)";
+
+    const String subnetMaskSlashNotationRegExpString = "^/([1-9]|[1-2][0-9]|3[0-1])\$";
+
+    return RegExp("^$subnetMaskRegExpPart\\.$subnetMaskRegExpPart\\.$subnetMaskRegExpPart\\.$subnetMaskRegExpPart\$").hasMatch(this.subnetMask)
+      || RegExp(subnetMaskSlashNotationRegExpString).hasMatch(this.subnetMask);
+  }
 }
