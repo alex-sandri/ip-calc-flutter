c60e94db23e6dbc10a2f77ddba990132d3d4eab3
Alex Sandri <me@alexsandri.com>
2020-08-18 14:48:32 +0200

    Add isValid method in IpAddress class

--- lib/ip_address.dart
+++ lib/ip_address.dart
@@ -10,4 +10,10 @@ class IpAddress
     @required this.address,
     this.subnetMask,
   });
+
+  bool isValid() {
+    const String ipv4RegExpPart = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+
+    return RegExp("^$ipv4RegExpPart\\.$ipv4RegExpPart\\.$ipv4RegExpPart\\.$ipv4RegExpPart\$").hasMatch(this.address);
+  }
 }
