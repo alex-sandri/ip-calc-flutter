80e3c9e066db397a6fb6f203d009275988763093
Alex Sandri <me@alexsandri.com>
2020-08-19 18:48:55 +0200

    Fix: overflow

--- lib/tabs/ip_address_info.dart
+++ lib/tabs/ip_address_info.dart
@@ -18,144 +18,141 @@ class _IpAddressInfoState extends State<IpAddressInfo> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return ListView(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomTextField(
-            label: "IP address",
-            hint: "192.168.1.1",
-            error: _ipAddressError,
-            controller: _ipAddressController,
-          ),
-          CustomTextField(
-            label: "Subnet mask",
-            hint: "255.255.255.0 or /24",
-            error: _subnetMaskError,
-            controller: _subnetMaskController,
-          ),
-          CustomFlatButton(
-            text: "Calc",
-            onPressed: () {
-              IpAddress ipAddress;
-              SubnetMask subnetMask;
+      children: [
+        CustomTextField(
+          label: "IP address",
+          hint: "192.168.1.1",
+          error: _ipAddressError,
+          controller: _ipAddressController,
+        ),
+        CustomTextField(
+          label: "Subnet mask",
+          hint: "255.255.255.0 or /24",
+          error: _subnetMaskError,
+          controller: _subnetMaskController,
+        ),
+        CustomFlatButton(
+          text: "Calc",
+          onPressed: () {
+            IpAddress ipAddress;
+            SubnetMask subnetMask;
 
-              try
-              {
-                subnetMask = SubnetMask(_subnetMaskController.text);
+            try
+            {
+              subnetMask = SubnetMask(_subnetMaskController.text);
 
-                _subnetMaskError = null;
-              }
-              catch (e)
-              {
-                if (e is ArgumentError)
-                  _subnetMaskError = e.message;
-              }
+              _subnetMaskError = null;
+            }
+            catch (e)
+            {
+              if (e is ArgumentError)
+                _subnetMaskError = e.message;
+            }
 
-              try
-              {
-                ipAddress = IpAddress(
-                  address: _ipAddressController.text,
-                  subnetMask: subnetMask,
-                );
-
-                _ipAddressError = null;
-              }
-              catch (e)
-              {
-                if (e is ArgumentError)
-                  _ipAddressError = e.message;
-              }
-
-              setState(() {});
-
-              if (_ipAddressError != null || _subnetMaskError != null) return;
-
-              showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SelectableText(
-                          "Network address",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        SelectableText(
-                          ipAddress.getNetworkAddress().address,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w100
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        SelectableText(
-                          "Broadcast address",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        SelectableText(
-                          ipAddress.getBroadcastAddress().address,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w100
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        SelectableText(
-                          "Maximum number of hosts",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        SelectableText(
-                          ipAddress.subnetMask.getMaxNumberOfHosts().toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.w100
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        SelectableText(
-                          "Private",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        SelectableText(
-                          ipAddress.isPrivate().toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.w100,
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
+            try
+            {
+              ipAddress = IpAddress(
+                address: _ipAddressController.text,
+                subnetMask: subnetMask,
               );
-            },
-          ),
-          CustomFlatButton(
-            text: "Reset",
-            onPressed: () {
-              _ipAddressController.text = _subnetMaskController.text = "";
 
-              setState(() {
-                _ipAddressError = _subnetMaskError = null;
-              });
-            },
-          ),
-        ],
-      ),
+              _ipAddressError = null;
+            }
+            catch (e)
+            {
+              if (e is ArgumentError)
+                _ipAddressError = e.message;
+            }
+
+            setState(() {});
+
+            if (_ipAddressError != null || _subnetMaskError != null) return;
+
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SelectableText(
+                        "Network address",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      SelectableText(
+                        ipAddress.getNetworkAddress().address,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w100
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      SelectableText(
+                        "Broadcast address",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      SelectableText(
+                        ipAddress.getBroadcastAddress().address,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w100
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      SelectableText(
+                        "Maximum number of hosts",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      SelectableText(
+                        ipAddress.subnetMask.getMaxNumberOfHosts().toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w100
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      SelectableText(
+                        "Private",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      SelectableText(
+                        ipAddress.isPrivate().toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
+        CustomFlatButton(
+          text: "Reset",
+          onPressed: () {
+            _ipAddressController.text = _subnetMaskController.text = "";
+
+            setState(() {
+              _ipAddressError = _subnetMaskError = null;
+            });
+          },
+        ),
+      ],
     );
   }
 }
--- lib/tabs/minimum_subnet_mask.dart
+++ lib/tabs/minimum_subnet_mask.dart
@@ -15,79 +15,76 @@ class _MinimumSubnetMaskState extends State<MinimumSubnetMask> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return ListView(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomTextField(
-            label: "Number of hosts needed",
-            hint: "254",
-            error: _numberOfHostsNeededError,
-            controller: _numberOfHostsNeededController,
-          ),
-          CustomFlatButton(
-            text: "Calc",
-            onPressed: () {
-              SubnetMask subnetMask;
+      children: [
+        CustomTextField(
+          label: "Number of hosts needed",
+          hint: "254",
+          error: _numberOfHostsNeededError,
+          controller: _numberOfHostsNeededController,
+        ),
+        CustomFlatButton(
+          text: "Calc",
+          onPressed: () {
+            SubnetMask subnetMask;
 
-              try
-              {
-                if (int.tryParse(_numberOfHostsNeededController.text) == null) throw ArgumentError("Invalid number of hosts");
+            try
+            {
+              if (int.tryParse(_numberOfHostsNeededController.text) == null) throw ArgumentError("Invalid number of hosts");
 
-                subnetMask = SubnetMask.getMinimum(int.parse(_numberOfHostsNeededController.text));
+              subnetMask = SubnetMask.getMinimum(int.parse(_numberOfHostsNeededController.text));
 
-                _numberOfHostsNeededError = null;
-              }
-              catch (e)
-              {
-                if (e is ArgumentError)
-                  _numberOfHostsNeededError = e.message;
-              }
+              _numberOfHostsNeededError = null;
+            }
+            catch (e)
+            {
+              if (e is ArgumentError)
+                _numberOfHostsNeededError = e.message;
+            }
 
-              setState(() {});
+            setState(() {});
 
-              if (_numberOfHostsNeededError != null) return;
+            if (_numberOfHostsNeededError != null) return;
 
-              showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SelectableText(
-                          "Subnet mask",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SelectableText(
+                        "Subnet mask",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
                         ),
-                        SelectableText(
-                          "${subnetMask.convertTo(SubnetMaskNotation.DOT_DECIMAL).subnetMask} (${subnetMask.convertTo(SubnetMaskNotation.SLASH).subnetMask})",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w100
-                          ),
+                      ),
+                      SelectableText(
+                        "${subnetMask.convertTo(SubnetMaskNotation.DOT_DECIMAL).subnetMask} (${subnetMask.convertTo(SubnetMaskNotation.SLASH).subnetMask})",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w100
                         ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            },
-          ),
-          CustomFlatButton(
-            text: "Reset",
-            onPressed: () {
-              _numberOfHostsNeededController.text = "";
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
+        CustomFlatButton(
+          text: "Reset",
+          onPressed: () {
+            _numberOfHostsNeededController.text = "";
 
-              setState(() {
-                _numberOfHostsNeededError = null;
-              });
-            },
-          ),
-        ],
-      ),
+            setState(() {
+              _numberOfHostsNeededError = null;
+            });
+          },
+        ),
+      ],
     );
   }
 }
