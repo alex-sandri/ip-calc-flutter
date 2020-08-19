978fb2915f98a6914c5877004cc26df2933134ca
Alex Sandri <me@alexsandri.com>
2020-08-19 18:48:11 +0200

    Fix: overflow

--- lib/tabs/vlsm.dart
+++ lib/tabs/vlsm.dart
@@ -18,75 +18,72 @@ class _VlsmState extends State<Vlsm> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return ListView(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomTextField(
-            label: "IP address",
-            hint: "192.168.1.1",
-            error: _ipAddressError,
-            controller: _ipAddressController,
-          ),
-          CustomTextField(
-            label: "Subnet mask",
-            hint: "255.255.255.0 or /24",
-            error: _subnetMaskError,
-            controller: _subnetMaskController,
-          ),
-          CustomTextField(
-            label: "Number of subnets",
-            hint: "4",
-            error: _numberOfSubnetsError,
-            controller: _numberOfSubnetsController,
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: int.tryParse(_numberOfSubnetsController.text) ?? 0,
-            itemBuilder: (context, index) {
-              final TextEditingController subnetNameController = TextEditingController(text: "Subnet$index");
+      children: [
+        CustomTextField(
+          label: "IP address",
+          hint: "192.168.1.1",
+          error: _ipAddressError,
+          controller: _ipAddressController,
+        ),
+        CustomTextField(
+          label: "Subnet mask",
+          hint: "255.255.255.0 or /24",
+          error: _subnetMaskError,
+          controller: _subnetMaskController,
+        ),
+        CustomTextField(
+          label: "Number of subnets",
+          hint: "4",
+          error: _numberOfSubnetsError,
+          controller: _numberOfSubnetsController,
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: int.tryParse(_numberOfSubnetsController.text) ?? 0,
+          itemBuilder: (context, index) {
+            final TextEditingController subnetNameController = TextEditingController(text: "Subnet$index");
 
-              return Row(
-                children: [
-                  Expanded(
-                    child: CustomTextField(
-                      label: "Subnet name",
-                      hint: "Subnet$index",
-                      controller: subnetNameController,
-                    ),
+            return Row(
+              children: [
+                Expanded(
+                  child: CustomTextField(
+                    label: "Subnet name",
+                    hint: "Subnet$index",
+                    controller: subnetNameController,
                   ),
-                  SizedBox(
-                    width: 8,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: CustomTextField(
+                    label: "Subnet size",
+                    hint: "10",
                   ),
-                  Expanded(
-                    child: CustomTextField(
-                      label: "Subnet size",
-                      hint: "10",
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-          CustomFlatButton(
-            text: "Calc",
-            onPressed: () {
-              // TODO
-            },
-          ),
-          CustomFlatButton(
-            text: "Reset",
-            onPressed: () {
-              _ipAddressController.text = _subnetMaskController.text = _numberOfSubnetsController.text = "";
+                ),
+              ],
+            );
+          },
+        ),
+        CustomFlatButton(
+          text: "Calc",
+          onPressed: () {
+            // TODO
+          },
+        ),
+        CustomFlatButton(
+          text: "Reset",
+          onPressed: () {
+            _ipAddressController.text = _subnetMaskController.text = _numberOfSubnetsController.text = "";
 
-              setState(() {
-                _ipAddressError = _subnetMaskError = _numberOfSubnetsError = null;
-              });
-            },
-          ),
-        ],
-      ),
+            setState(() {
+              _ipAddressError = _subnetMaskError = _numberOfSubnetsError = null;
+            });
+          },
+        ),
+      ],
     );
   }
 }
