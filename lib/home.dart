514efc774150ac3a46eb93b22738c37aa4899a1c
Alex Sandri <me@alexsandri.com>
2020-09-10 13:25:29 +0200

    Add quick actions for every tab

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,44 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:ip_calc/tabs/ip_address_info.dart';
 import 'package:ip_calc/tabs/minimum_subnet_mask.dart';
 import 'package:ip_calc/tabs/vlsm.dart';
+import 'package:quick_actions/quick_actions.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    final TabController tabController = TabController(
+      length: 3,
+      vsync: this,
+    );
+
+    if (Platform.isAndroid || Platform.isIOS)
+    {
+      final QuickActions quickActions = QuickActions();
+
+      quickActions.initialize((shortcutType) {
+        switch (shortcutType)
+        {
+          case "action_ip_address_info": tabController.index = 0; break;
+          case "action_min_subnet_mask": tabController.index = 1; break;
+          case "action_vlsm": tabController.index = 2; break;
+        }
+      });
+
+      quickActions.setShortcutItems([
+        const ShortcutItem(type: "action_ip_address_info", localizedTitle: "IP address info", icon: "ic_launcher"),
+        const ShortcutItem(type: "action_min_subnet_mask", localizedTitle: "Minimum subnet mask", icon: "ic_launcher"),
+        const ShortcutItem(type: "action_vlsm", localizedTitle: "VLSM", icon: "ic_launcher"),
+      ]);
+    }
+
     return Material(
       child: GestureDetector(
         onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
@@ -17,6 +50,7 @@ class Home extends StatelessWidget {
                 "IP Calc",
               ),
               bottom: TabBar(
+                controller: tabController,
                 isScrollable: true,
                 tabs: [
                   Tab(
