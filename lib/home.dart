f7b1e7835afc186ed5f8d2cf5a04f22b8c7637bc
Alex Sandri <me@alexsandri.com>
2020-09-11 09:01:34 +0200

    Fix: tabs do not change

--- lib/home.dart
+++ lib/home.dart
@@ -6,72 +6,68 @@ import 'package:ip_calc/tabs/minimum_subnet_mask.dart';
 import 'package:ip_calc/tabs/vlsm.dart';
 import 'package:quick_actions/quick_actions.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final TabController tabController = TabController(
-      length: 3,
-      vsync: this,
-    );
-
-    if (Platform.isAndroid || Platform.isIOS)
-    {
-      final QuickActions quickActions = QuickActions();
-
-      quickActions.initialize((shortcutType) {
-        switch (shortcutType)
-        {
-          case "action_ip_address_info": tabController.index = 0; break;
-          case "action_min_subnet_mask": tabController.index = 1; break;
-          case "action_vlsm": tabController.index = 2; break;
-        }
-      });
-
-      quickActions.setShortcutItems([
-        const ShortcutItem(type: "action_ip_address_info", localizedTitle: "IP address info", icon: "ic_launcher"),
-        const ShortcutItem(type: "action_min_subnet_mask", localizedTitle: "Minimum subnet mask", icon: "ic_launcher"),
-        const ShortcutItem(type: "action_vlsm", localizedTitle: "VLSM", icon: "ic_launcher"),
-      ]);
-    }
-
     return Material(
       child: GestureDetector(
         onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
         child: DefaultTabController(
           length: 3,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text(
-                "IP Calc",
-              ),
-              bottom: TabBar(
-                controller: tabController,
-                isScrollable: true,
-                tabs: [
-                  Tab(
-                    text: "IP Address Info",
+          child: Builder(
+            builder: (context) {
+              final TabController tabController = DefaultTabController.of(context);
+
+              if (Platform.isAndroid || Platform.isIOS)
+              {
+                final QuickActions quickActions = QuickActions();
+
+                quickActions.initialize((shortcutType) {
+                  switch (shortcutType)
+                  {
+                    case "action_ip_address_info": tabController.index = 0; break;
+                    case "action_min_subnet_mask": tabController.index = 1; break;
+                    case "action_vlsm": tabController.index = 2; break;
+                  }
+                });
+
+                quickActions.setShortcutItems([
+                  const ShortcutItem(type: "action_ip_address_info", localizedTitle: "IP address info", icon: "ic_launcher"),
+                  const ShortcutItem(type: "action_min_subnet_mask", localizedTitle: "Minimum subnet mask", icon: "ic_launcher"),
+                  const ShortcutItem(type: "action_vlsm", localizedTitle: "VLSM", icon: "ic_launcher"),
+                ]);
+              }
+
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(
+                    "IP Calc",
                   ),
-                  Tab(
-                    text: "Minimum Subnet Mask",
+                  bottom: TabBar(
+                    controller: tabController,
+                    isScrollable: true,
+                    tabs: [
+                      Tab(
+                        text: "IP Address Info",
+                      ),
+                      Tab(
+                        text: "Minimum Subnet Mask",
+                      ),
+                      Tab(
+                        text: "Variable Length Subnet Mask (VLSM)",
+                      ),
+                    ],
                   ),
-                  Tab(
-                    text: "Variable Length Subnet Mask (VLSM)",
-                  ),
-                ],
-              ),
-            ),
-            body: TabBarView(
-              children: [
-                IpAddressInfo(),
-                MinimumSubnetMask(),
-                Vlsm(),
-              ],
-            ),
+                ),
+                body: TabBarView(
+                  children: [
+                    IpAddressInfo(),
+                    MinimumSubnetMask(),
+                    Vlsm(),
+                  ],
+                ),
+              );
+            },
           ),
         ),
       ),
